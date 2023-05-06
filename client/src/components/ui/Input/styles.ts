a94f83cf0ee66f9614925743a163a20a1f5e053f
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-05-06 18:58:08 -0300

    :ok_hand: style: fix prettier style

--- client/src/components/ui/Input/styles.ts
+++ client/src/components/ui/Input/styles.ts
@@ -1,19 +1,22 @@
-import styled from "styled-components";
-import { IonInput  } from "@ionic/react";
+import styled from 'styled-components'
+import { IonInput } from '@ionic/react'
 
-interface InputProps  {
+interface InputProps {
   kind?: 'primary' | 'secondary'
 }
 
 const Input = styled(IonInput)`
-  --highlight-color-focused: ${({kind}:InputProps) => kind === "primary" ? "#000000" : "#AC884D"};
+  --highlight-color-focused: ${({ kind }: InputProps) =>
+    kind === 'primary' ? '#000000' : '#AC884D'};
   --border-width: 1px;
   --border-style: solid;
-  border-bottom: ${({kind}:InputProps) => kind === "primary" ? "1px solid #000000" : "1px solid #AC884D"};
+  border-bottom: ${({ kind }: InputProps) =>
+    kind === 'primary' ? '1px solid #000000' : '1px solid #AC884D'};
   --placeholder-color: white;
   --placeholder-font-style: normal;
   --placeholder-font-weight: 400;
   --placeholder-font-size: 12px;
-`;
+  color: white;
+`
 
-export { Input };
+export { Input }
