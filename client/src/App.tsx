f3001b8ec4d8fa2fa7b5e97595275da5a302a06e
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-05-06 17:43:42 -0300

    :recycle: refactor: refatorando a App.tsx com as novas rotas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
-import { Redirect, Route } from 'react-router-dom'
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react'
-import { IonReactRouter } from '@ionic/react-router'
+import { IonApp, setupIonicReact } from '@ionic/react'
 
-import Home from './pages/Home'
+import Routes from './routes'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css'
@@ -18,16 +16,7 @@ setupIonicReact()
 const App: React.FC = () => (
   <IonApp>
     <GlobalStyle />
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path='/home'>
-          <Home />
-        </Route>
-        <Route exact path='/'>
-          <Redirect to='/home' />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
+    <Routes />
   </IonApp>
 )
 
