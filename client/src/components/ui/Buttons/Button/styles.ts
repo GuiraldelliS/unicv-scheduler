bc9081a41d85c333d46851569d6abc2e33cc2af6
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-05-06 18:56:14 -0300

    :ok_hand: style: correção style prettier

--- client/src/components/ui/Buttons/Button/styles.ts
+++ client/src/components/ui/Buttons/Button/styles.ts
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import { IonButton } from '@ionic/react';
+import styled from 'styled-components'
+import { IonButton } from '@ionic/react'
 
 type ButtonProps = {
   kind: 'primary' | 'secondary'
@@ -8,16 +8,17 @@ type ButtonProps = {
 
 const Button = styled(IonButton)`
   width: 100%;
-  --background: ${({kind, customColor}:ButtonProps) => customColor ? customColor : kind === "primary" ? "#4A5A3B" : "#FFFFFF"};
-  --color: ${({kind}:ButtonProps) => kind === "primary" ? "#FFFFFF" : "#000000"};
+  --background: ${({ kind, customColor }: ButtonProps) =>
+    customColor ? customColor : kind === 'primary' ? '#4A5A3B' : '#FFFFFF'};
+  --color: ${({ kind }: ButtonProps) =>
+    kind === 'primary' ? '#FFFFFF' : '#000000'};
   --border-radius: 10px;
   font-weight: 400;
   font-size: 1rem;
   line-height: 18px;
   text-transform: none;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-`;
-
-
+  min-height: 48px;
+`
 
 export { Button }
