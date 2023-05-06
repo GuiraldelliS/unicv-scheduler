acf29e0f20a6b1507fb72dfa78feba81774243f1
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-05-06 19:39:46 -0300

    :bug: fix: correção no redirect login > register

--- client/src/pages/auth/login/index.tsx
+++ client/src/pages/auth/login/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
 import { IonContent, IonPage } from '@ionic/react'
-import * as Styles from './styles'
+import { useHistory } from 'react-router-dom'
+
 import Input from '../../../components/ui/Input'
 import { Button } from '../../../components/ui/Buttons'
-
 import { LogoIcon } from '../../../components/ui/Icons'
 
+import * as Styles from './styles'
+
 const Login = () => {
+  const history = useHistory()
   return (
     <IonPage>
       <IonContent>
@@ -19,7 +21,14 @@ const Login = () => {
             <Input placeholder='Digite seu cpf ou e-mail' />
             <Input placeholder='Digite sua senha' type='password' />
             <Button customColor='#7B9165'>Entrar</Button>
-            <Button kind='secondary'>Registrar</Button>
+            <Button
+              kind='secondary'
+              onClick={(e: any) => {
+                e.preventDefault()
+                history.push('/register')
+              }}>
+              Registrar
+            </Button>
           </Styles.WrapperForm>
         </Styles.Container>
       </IonContent>
