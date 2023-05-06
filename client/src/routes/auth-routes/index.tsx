267ab62672b2626bf0875ec1baf505a64338857d
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-05-06 18:59:03 -0300

    :sparkles: feat: adiciona estrutura para a tela de register

--- client/src/routes/auth-routes/index.tsx
+++ client/src/routes/auth-routes/index.tsx
@@ -4,6 +4,9 @@ import { IonRouterOutlet } from '@ionic/react'
 import { withLazyLoading } from '../utils/withLazyLoading'
 
 const Login = withLazyLoading(lazy(() => import('../../pages/auth/login')))
+const Register = withLazyLoading(
+  lazy(() => import('../../pages/auth/register'))
+)
 
 const AuthRoutes = () => {
   return (
@@ -13,6 +16,9 @@ const AuthRoutes = () => {
       </Route>
       <Route exact path='/'>
         <Redirect to='/login' />
+      </Route>
+      <Route exact path='/register'>
+        <Register />
       </Route>
     </IonRouterOutlet>
   )
