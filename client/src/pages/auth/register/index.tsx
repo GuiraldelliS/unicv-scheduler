97aa686c7510dc54608cb93000a0e4b235129d52
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-05-06 19:40:08 -0300

    :sparkles: feat: adiciona design tela register

--- client/src/pages/auth/register/index.tsx
+++ client/src/pages/auth/register/index.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import { IonContent, IonPage } from '@ionic/react'
+import { Link } from 'react-router-dom'
+
+import Input from '../../../components/ui/Input'
+import { Button } from '../../../components/ui/Buttons'
+
+import * as Styles from './styles'
+import { ArrowBackIcon } from '../../../components/ui/Icons/ArrowBackIcon'
 
 const Register = () => {
-  return <div>Register</div>
+  return (
+    <IonPage>
+      <IonContent>
+        <Styles.ContainerActions>
+          <Link to='/login'>
+            <ArrowBackIcon color='#425B18' />
+          </Link>
+        </Styles.ContainerActions>
+        <Styles.WrapperTitle>
+          <h1>Cadastre-se</h1>
+          <h5>Informe os dados necessários</h5>
+        </Styles.WrapperTitle>
+        <Styles.Container>
+          <Styles.WrapperForm>
+            <Input label='Nome' kind='secondary' />
+            <Input label='E-mail' kind='secondary' />
+            <Input label='Telefone' kind='secondary' />
+            <Input label='Senha' type='password' kind='secondary' />
+            <Button kind='secondary'>Cadastrar</Button>
+          </Styles.WrapperForm>
+        </Styles.Container>
+      </IonContent>
+    </IonPage>
+  )
 }
 
 export default Register
