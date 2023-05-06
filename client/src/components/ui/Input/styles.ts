b4afe07758f4b75e9509c49999795650057ec11f
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-05-06 15:34:38 -0300

    :sparkles: feat: adiciona componente Input

---
+++ client/src/components/ui/Input/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { IonInput  } from "@ionic/react";
+
+interface InputProps  {
+  kind?: 'primary' | 'secondary'
+}
+
+const Input = styled(IonInput)`
+  --highlight-color-focused: ${({kind}:InputProps) => kind === "primary" ? "#000000" : "#AC884D"};
+  --border-width: 1px;
+  --border-style: solid;
+  border-bottom: ${({kind}:InputProps) => kind === "primary" ? "1px solid #000000" : "1px solid #AC884D"};
+  --placeholder-color: white;
+  --placeholder-font-style: normal;
+  --placeholder-font-weight: 400;
+  --placeholder-font-size: 12px;
+`;
+
+export { Input };
