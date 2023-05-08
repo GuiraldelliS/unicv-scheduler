2ed40850c57487c41d8a7bf3a380e53a249c3f90
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-05-08 16:13:33 -0300

    :bug: fix: remove design das telas de auth
    contexto:
    
    vai ter mudanças no figma do sistema devido a isso a remoção é necessaria

--- client/src/pages/auth/login/index.tsx
+++ client/src/pages/auth/login/index.tsx
@@ -1,36 +1,12 @@
 import { IonContent, IonPage } from '@ionic/react'
 import { useHistory } from 'react-router-dom'
 
-import Input from '../../../components/ui/Input'
-import { Button } from '../../../components/ui/Buttons'
-import { LogoIcon } from '../../../components/ui/Icons'
-
-import * as Styles from './styles'
-
 const Login = () => {
   const history = useHistory()
   return (
     <IonPage>
       <IonContent>
-        <Styles.WrapperTitle>
-          <LogoIcon size='32' color='red' />
-          <h1>| UNIDULE</h1>
-        </Styles.WrapperTitle>
-        <Styles.Container>
-          <Styles.WrapperForm onSubmit={(e: any) => e.preventDefault()}>
-            <Input placeholder='Digite seu cpf ou e-mail' />
-            <Input placeholder='Digite sua senha' type='password' />
-            <Button customColor='#7B9165'>Entrar</Button>
-            <Button
-              kind='secondary'
-              onClick={(e: any) => {
-                e.preventDefault()
-                history.push('/register')
-              }}>
-              Registrar
-            </Button>
-          </Styles.WrapperForm>
-        </Styles.Container>
+        <div>Login</div>
       </IonContent>
     </IonPage>
   )
--- client/src/pages/auth/register/index.tsx
+++ client/src/pages/auth/register/index.tsx
@@ -1,34 +1,10 @@
 import { IonContent, IonPage } from '@ionic/react'
-import { Link } from 'react-router-dom'
-
-import Input from '../../../components/ui/Input'
-import { Button } from '../../../components/ui/Buttons'
-
-import * as Styles from './styles'
-import { ArrowBackIcon } from '../../../components/ui/Icons/ArrowBackIcon'
 
 const Register = () => {
   return (
     <IonPage>
       <IonContent>
-        <Styles.ContainerActions>
-          <Link to='/login'>
-            <ArrowBackIcon color='#425B18' />
-          </Link>
-        </Styles.ContainerActions>
-        <Styles.WrapperTitle>
-          <h1>Cadastre-se</h1>
-          <h5>Informe os dados necessários</h5>
-        </Styles.WrapperTitle>
-        <Styles.Container>
-          <Styles.WrapperForm onSubmit={(e: any) => e.preventDefault()}>
-            <Input label='Nome' kind='secondary' />
-            <Input label='E-mail' kind='secondary' />
-            <Input label='Telefone' kind='secondary' />
-            <Input label='Senha' type='password' kind='secondary' />
-            <Button kind='secondary'>Cadastrar</Button>
-          </Styles.WrapperForm>
-        </Styles.Container>
+        <div>Register</div>
       </IonContent>
     </IonPage>
   )
