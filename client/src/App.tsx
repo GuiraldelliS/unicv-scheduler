011ea2d069c6f9d1f38871a2b6850a8088a6f740
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-05-08 16:12:03 -0300

    :wrench: chore: altera a estrutura para utilizar o baseweb

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 import { IonApp, setupIonicReact } from '@ionic/react'
+import { Provider as StyletronProvider } from 'styletron-react'
+import { BaseProvider } from 'baseui'
 
 import Routes from './routes'
 
@@ -9,14 +11,28 @@ import '@ionic/react/css/normalize.css'
 import '@ionic/react/css/structure.css'
 import '@ionic/react/css/typography.css'
 
-import GlobalStyle from './theme/global'
+import './styles/global.css'
+import { AlertContextProvider } from './contexts/AlertContext'
+import { LightTheme } from './themes/light-theme'
+import { styletron } from './styletron'
 
 setupIonicReact()
 
+const StyleContextProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StyletronProvider value={styletron}>
+      <BaseProvider theme={LightTheme}>{children}</BaseProvider>
+    </StyletronProvider>
+  )
+}
+
 const App: React.FC = () => (
   <IonApp>
-    <GlobalStyle />
-    <Routes />
+    <StyleContextProviders>
+      <AlertContextProvider>
+        <Routes />
+      </AlertContextProvider>
+    </StyleContextProviders>
   </IonApp>
 )
 
--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,4 +1,3 @@
-import { useReducer } from 'react'
 import {
   IonContent,
   IonHeader,
@@ -7,10 +6,51 @@ import {
   IonToolbar,
 } from '@ionic/react'
 import { Button } from '../../components/ui/Buttons'
-import Input from '../../components/ui/Input'
+import Input from '../../components/ui/Form/Input'
+import * as Yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import { Heading, Paragraph } from '../../components/ui/Typography'
+import { useAlert } from '../../contexts/AlertContext'
+import Select from '../../components/ui/Form/Select'
+import Textarea from '../../components/ui/Form/Textarea'
+import Section from '../../components/layout/Section'
+import { useForm } from 'react-hook-form'
+
+const schema = Yup.object().shape({
+  password: Yup.string()
+    .required('Senha atual é obrigatória')
+    .min(6, 'Mínimo de 6 caracteres'),
+})
 
 const Home: React.FC = () => {
-  const [open, toggleOpen] = useReducer((open) => !open, false)
+  const alert = useAlert()
+
+  const { control, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'all',
+    reValidateMode: 'onChange',
+  })
+
+  const handleTestAlert = () => {
+    try {
+      alert.open({
+        status: 'error',
+        title: 'Sucesso!',
+        message: 'Sua solicitação foi enviada com sucesso!',
+      })
+    } catch (error) {
+      alert.open({
+        status: 'error',
+        title: 'ERRO!',
+        message: 'Sua solicitação foi enviada com sucesso!',
+      })
+    }
+  }
+
+  const handleTestYupValidation = handleSubmit((data) => {
+    console.log(data)
+  })
   return (
     <IonPage>
       <IonHeader>
@@ -25,8 +65,35 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <>
-          <Button onClick={toggleOpen}>Primary</Button>
-          <Input kind='secondary' placeholder='CPF' />
+          <Section>
+            <Button onClick={handleTestYupValidation}>Primary</Button>
+            <Input placeholder='CPF' mask='999.999.999-99' />
+            <Input
+              placeholder='senha'
+              name='password'
+              control={control}
+              type='password'
+            />
+            <Select
+              options={[
+                { label: 'AliceBlue', id: '#F0F8FF' },
+                { label: 'AntiqueWhite', id: '#FAEBD7' },
+                { label: 'Aqua', id: '#00FFFF' },
+                { label: 'Aquamarine', id: '#7FFFD4' },
+                { label: 'Azure', id: '#F0FFFF' },
+                { label: 'Beige', id: '#F5F5DC' },
+              ]}
+            />
+            <Textarea placeholder='Textarea' />
+            <Paragraph.XSmall>Paragraph XSmall</Paragraph.XSmall>
+            <Paragraph.Small>Paragraph Small</Paragraph.Small>
+            <Paragraph.Medium>Paragraph Medium</Paragraph.Medium>
+            <Paragraph.Large>Paragraph Large</Paragraph.Large>
+            <Heading.XSmall>Heading XSmall</Heading.XSmall>
+            <Heading.Small>Heading Small</Heading.Small>
+            <Heading.Medium>Heading Medium</Heading.Medium>
+            <Heading.Large>Heading Large</Heading.Large>
+          </Section>
         </>
       </IonContent>
     </IonPage>
