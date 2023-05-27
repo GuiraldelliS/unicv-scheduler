8c9f854e41d83b642c0cce580a3e2094cb52e391
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-05-27 09:58:08 -0300

    fix: corrige erros de typescript

--- client/src/routes/auth-routes/index.tsx
+++ client/src/routes/auth-routes/index.tsx
@@ -3,21 +3,26 @@ import { Redirect, Route } from 'react-router-dom'
 import { IonRouterOutlet } from '@ionic/react'
 import { withLazyLoading } from '../utils/withLazyLoading'
 
-const Login = withLazyLoading(lazy(() => import('../../pages/auth/login')))
+const Login = withLazyLoading(
+  lazy(() => import('../../pages/auth/login' as any))
+)
 const Register = withLazyLoading(
   lazy(() => import('../../pages/auth/register'))
+)
+const SplashScreen = withLazyLoading(
+  lazy(() => import('../../pages/auth/splashScreen'))
 )
 
 const AuthRoutes = () => {
   return (
     <IonRouterOutlet>
-      <Route exact path='/login'>
+      <Route path='/login'>
         <Login />
       </Route>
       <Route exact path='/'>
-        <Redirect to='/login' />
+        <SplashScreen />
       </Route>
-      <Route exact path='/register'>
+      <Route path='/register'>
         <Register />
       </Route>
     </IonRouterOutlet>
