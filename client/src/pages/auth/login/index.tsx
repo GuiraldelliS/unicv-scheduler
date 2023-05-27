b002084852fe326b06cee2cd0f4e9ead879398c9
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-05-27 09:57:29 -0300

    feat: adiciona design tela de registro e login

--- client/src/pages/auth/login/index.tsx
+++ client/src/pages/auth/login/index.tsx
@@ -1,15 +1,56 @@
 import { IonContent, IonPage } from '@ionic/react'
-import { useHistory } from 'react-router-dom'
+import { withRouter } from 'react-router'
+import { Block } from 'baseui/block'
 
-const Login = () => {
-  const history = useHistory()
+import Header from '../../../components/layout/Header'
+import { Heading } from '../../../components/ui/Typography'
+import Input from '../../../components/ui/Form/Input'
+import { Button } from '../../../components/ui/Buttons'
+import { useCallback } from 'react'
+
+const Login = ({ history }) => {
+  const handleRedirectToRegister = () => {
+    history.push('/register')
+  }
+
   return (
     <IonPage>
       <IonContent>
-        <div>Login</div>
+        <Header />
+        <Block
+          display='flex'
+          flexDirection='column'
+          gridColumnGap='8px'
+          padding='8px'>
+          <Block
+            display='flex'
+            flexDirection='column'
+            gridColumnGap='16px'
+            marginTop='16px'
+            marginBottom='16px'>
+            <Heading.Medium color='#6F738A'>Acesse</Heading.Medium>
+            <Heading.XSmall color='#6F738A'>
+              Utilize o e-mail e senha para entrar
+            </Heading.XSmall>
+          </Block>
+          <Input label='Digite seu e-mail' />
+          <Input label='Digite sua senha' type='password' />
+          <Block display='flex' gridColumnGap='16px' marginTop='2rem'>
+            <Button
+              kind='outline'
+              size='large'
+              fullWidth
+              onClick={handleRedirectToRegister}>
+              Cadastrar
+            </Button>
+            <Button size='large' fullWidth>
+              Acessar
+            </Button>
+          </Block>
+        </Block>
       </IonContent>
     </IonPage>
   )
 }
 
-export default Login
+export default withRouter(Login)
