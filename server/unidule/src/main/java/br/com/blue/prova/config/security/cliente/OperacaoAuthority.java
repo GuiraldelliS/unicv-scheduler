bae0bf7f148ad722cd8cc0daf03f13e4c295a38c
Matheus Miriani <matheusmiriani@hotmail.com>
2023-06-09 19:49:38 -0300

    ajustando projeto

--- server/unidule/src/main/java/br/com/blue/prova/config/security/cliente/OperacaoAuthority.java
+++
@@ -1,8 +0,0 @@
-package br.com.blue.prova.config.security.cliente;
-
-public enum OperacaoAuthority {
-    READ,
-    CREATE,
-    UPDATE,
-    DELETE
-}
--- server/unidule/src/main/java/br/com/blue/prova/config/security/cliente/Scope.java
+++
@@ -1,8 +0,0 @@
-package br.com.blue.prova.config.security.cliente;
-
-public class Scope {
-    public static final String SISTEMA = "'sistema'";
-
-    private Scope() {
-    }
-}
