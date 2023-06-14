db22be0f2ae9e719a2b165d82965dd3bc769f036
Matheus Miriani <88916107+mathelsz@users.noreply.github.com>
2023-06-14 00:26:23 -0300

    feat: create classes on package repository

--- server/unidule/src/main/java/br/com/blue/prova/repository/UserMasterRepository.java
+++ server/unidule/src/main/java/br/com/blue/prova/repository/UserMasterRepository.java
@@ -1,10 +1,10 @@
 package br.com.blue.prova.repository;
 
-import br.com.blue.prova.domain.User;
+import br.com.blue.prova.domain.UserMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+public interface UserMasterRepository extends JpaRepository<UserMaster, Long>, JpaSpecificationExecutor<UserMaster> {
 }
