c2d2444f9cb16ef6e8e1b6421bbd61fff77a9c1a
Matheus Miriani <88916107+mathelsz@users.noreply.github.com>
2023-06-16 22:24:38 -0300

    feat: create query

--- server/prova/src/main/java/br/com/blue/prova/repository/AppointmentRepository.java
+++ server/prova/src/main/java/br/com/blue/prova/repository/AppointmentRepository.java
@@ -3,8 +3,20 @@ package br.com.blue.prova.repository;
 import br.com.blue.prova.domain.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>, JpaSpecificationExecutor<Appointment> {
+
+    @Query(value = """
+            select count(id) > 0 from appointment where start_time = :startTime and
+            end_time = :endTime and
+            "date" = :date and
+            professional_id = :professionalId
+            """, nativeQuery = true)
+    Boolean checkAppointmentAvailability(LocalTime startTime, LocalTime endTime, LocalDate date, Long professionalId);
 }
